9626a237025f64bce5f1577786c9683dfc2883dd
Alberto Di Prinzio <alberto.diprinzio@itsincom.it>
2025-03-13 16:08:57 +0100

    Dbcontext e connstring aggiunte :)

---
+++ EsercitazioneControlloAccessi/ControlloAccessiDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EsercitazioneControlloAccessi.Entities;
+namespace EsercitazioneControlloAccessi
+{
+    public class ControlloAccessiDbContext : DbContext
+    {
+
+       
+            public ControlloAccessiDbContext() : base() { }
+
+            public ControlloAccessiDbContext(DbContextOptions<ControlloAccessiDbContext> options)
+                : base(options) { }
+
+            public DbSet<Dipendente> Dipendenti { get; set; }
+            public DbSet<Ruolo> Ruoli { get; set; }
+            public DbSet<Azienda> Aziende { get; set; }
+
+            public DbSet<Visita> Visite { get; set; }
+
+           public DbSet<Wifi> Wifi { get; set; }
+           public DbSet<PostoAuto> PostiAuto { get; set; }
+           public DbSet<Ospite> Ospiti
+        {
+            get; set;
+        }
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
--- EsercitazioneControlloAccessi/Program.cs
+++ EsercitazioneControlloAccessi/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+string connStr = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddSqlServer<ControlloAccessiDbContext>(connStr);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
