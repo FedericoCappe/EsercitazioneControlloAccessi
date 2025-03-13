eff3d77332e1d5f74960b7ad813d5628f3c8a35e
chromos0 <saravollalorenzo@gmail.com>
2025-03-13 17:14:19 +0100

    fixed dbContext

--- EsercitazioneControlloAccessi/ControlloAccessiDbContext.cs
+++ EsercitazioneControlloAccessi/ControlloAccessiDbContext.cs
@@ -4,34 +4,19 @@ namespace EsercitazioneControlloAccessi
 {
     public class ControlloAccessiDbContext : DbContext
     {
+        public ControlloAccessiDbContext() : base() { }
 
-       
-            public ControlloAccessiDbContext() : base() { }
+        public ControlloAccessiDbContext(DbContextOptions<ControlloAccessiDbContext> options)
+            : base(options) { }
 
-            public ControlloAccessiDbContext(DbContextOptions<ControlloAccessiDbContext> options)
-                : base(options) { }
+        public DbSet<Dipendente> Dipendenti { get; set; }
+        public DbSet<Ruolo> Ruoli { get; set; }
+        public DbSet<AziendaOspite> Aziende { get; set; }
 
-            public DbSet<Dipendente> Dipendenti { get; set; }
-            public DbSet<Ruolo> Ruoli { get; set; }
-            public DbSet<AziendaOspite> Aziende { get; set; }
+        public DbSet<Visita> Visite { get; set; }
 
-            public DbSet<Visita> Visite { get; set; }
-
-           public DbSet<Wifi> Wifi { get; set; }
-           public DbSet<PostoAuto> PostiAuto { get; set; }
-           public DbSet<Ospite> Ospiti
-        {
-            get; set;
-        }
-
-
-
-
-
-        }
-
-
-
-
+        public DbSet<Wifi> Wifi { get; set; }
+        public DbSet<PostoAuto> PostiAuto { get; set; }
+        public DbSet<Ospite> Ospiti { get; set; }
     }
 }
